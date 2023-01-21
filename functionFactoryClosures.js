6e30386209645f49bc38b2307967004fcbe6fbb0
Owner <komalgill0310@gmail.com>
2023-01-21 14:12:30 -0500

    Closures => return a child function which does the addition of the parent arg and its own argument & return child function in parent function

---
+++ functionFactoryClosures.js
@@ -0,0 +1,8 @@
+function makePlusFunction(baseNum) {
+  function sumFunction(num) {
+    return baseNum + num;
+  }
+  return sumFunction;
+}
+
+// Problem link: https://edabit.com/challenge/ENWFBL4jbTgLbSqwS
