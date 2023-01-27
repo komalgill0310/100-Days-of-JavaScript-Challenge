8914d6312ec790489ca5b3aa7a781ec84c2583bb
Owner <komalgill0310@gmail.com>
2023-01-27 12:38:03 -0500

    Challenge: Do the addition of the all the object's property and return the sum

---
+++ sumOfObjectPropertiesValues.js
@@ -0,0 +1,5 @@
+function getTotalPrice(groceries) {
+  const totalPrice = groceries.reduce((a, c) => a + c.quantity * c.price, 0);
+  return +Number.parseFloat(totalPrice).toFixed(1);
+}
+// Problem link: https://edabit.com/challenge/jRSST87NjECBzbwzL
