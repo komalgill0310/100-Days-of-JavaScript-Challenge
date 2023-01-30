39597d7a5653e3d4dee29dcb3d133cb9938fce0f
Owner <komalgill0310@gmail.com>
2023-01-30 12:30:37 -0500

    Challenge: return unique number from an array

---
+++ findSingleNumber.js
@@ -0,0 +1,7 @@
+var singleNumber = function (nums) {
+  return nums.filter((num) => nums.indexOf(num) === nums.lastIndexOf(num));
+};
+
+// YET TO LEARN ALGORITHMS.
+
+// Problem link: https://leetcode.com/problems/single-number/
