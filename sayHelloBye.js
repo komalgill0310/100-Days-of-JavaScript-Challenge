7fa5af9bf1e3a25cea8394632ff91a69a848c9b3
Owner <komalgill0310@gmail.com>
2023-02-03 12:01:06 -0500

    Challenge: return hello or bye based on truthy or falsy value

---
+++ sayHelloBye.js
@@ -0,0 +1,7 @@
+function sayHelloBye(name, num) {
+  return num
+    ? `Hello ${name[0].toUpperCase() + name.slice(1)}`
+    : `Bye ${name[0].toUpperCase() + name.slice(1)}`;
+}
+
+// Problem link: https://edabit.com/challenge/kLa2w7m4h849k8rmW
