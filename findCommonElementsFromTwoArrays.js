a530910cf61d3d8b79ade5fe56fd67a0b8f0c702
Owner <komalgill0310@gmail.com>
2023-02-13 14:57:27 -0500

    Challenge: find common unique elements from two arrays

---
+++ findCommonElementsFromTwoArrays.js
@@ -0,0 +1,19 @@
+// Given: Two arrays of integers are given
+// Return: Find common elements which are in both arrays and return those elements in an array.
+
+// BREAKDOWN:
+// 1. use filter method on array1 and store the result in a variable called "commonElementsArr"
+// PARAMETERS: current number => num
+// ==> Inside filter check if array2 includes the current number
+
+// 2. return commonElementsArr
+
+// EDGE CASE: if any of the array has duplicates elements, then return array should only include unique elements.
+// 1. use Set on commonElementsArr in order to get rid of the duplicates
+//2. Use Spread Operator on a set object in order to convert the object into an array and perform this operation inside "[]" brackets so the output we get will be in form of an array.
+// => return [...new Set(commonElementsArr)]
+
+function commonElements(arr1, arr2) {
+  const commonElementsArr = arr1.filter((num) => arr2.includes(num));
+  return [...new Set(commonElementsArr)];
+}
