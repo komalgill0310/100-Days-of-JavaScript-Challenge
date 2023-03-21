bff32cd91d8aaa96570d28c4d15ef113a72d227f
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-03-21 17:12:48 -0400

    Update: solve the assign cookies problem

--- assignCookies.js
+++ assignCookies.js
@@ -12,34 +12,22 @@
 // 2. Output:
 // ==> Return how many children can you make them satisfy by fulfilling their desire for cookies.
 
-// Breakdown:
-// 1. Create a variable called "maxNumOfChildrenContent" and initialize it to 0
-// 2. const arr = g.length > s.length ? g : s
-// 2. Use for loop to loop through an array which has the smaller size
-// 3. Check if i > desire of cookie for each children.length
-// ==> return maxNumOfChildrenContent
-// 4. if (g.slice(i).find(desiredCookies => s[i] >= desiredCookies))
-// =====> maxNumOfChildrenContent++
-// 5. return maxNumOfChildrenContent
-
-// another function to return an array which has smaller length
-
 var findContentChildren = function (g, s) {
   let maxNumOfChildrenContent = 0;
-  const smallerLengtharr = g.length < s.length ? g : s;
-  const biggerLengthArr = smallerLengtharr === g ? s : g;
-  for (let i = 0; i < smallerLengtharr; i++) {
-    if (
-      biggerLengthArr
-        .slice(i)
-        .find((desiredCookies) => smallerLengtharr[i] >= desiredCookies)
-    ) {
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  let i = 0,
+    j = 0;
+  while (i < g.length && j < s.length) {
+    if (s[j] >= g[i]) {
+      i++;
+      j++;
       maxNumOfChildrenContent++;
+    } else {
+      j++;
     }
   }
   return maxNumOfChildrenContent;
 };
-
-// SOME TEST CASES ARE FAILING => YET TO FIGURE OUT
 
 // Problem link: https://leetcode.com/problems/assign-cookies/
