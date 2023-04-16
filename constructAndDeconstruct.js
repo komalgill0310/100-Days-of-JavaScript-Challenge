1d3da74cd56e9c29c84b60b9fbb83110baf51ad9
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-04-15 23:21:31 -0400

    Edit the breakdown for the construct and deconstruct problem

--- constructAndDeconstruct.js
+++ constructAndDeconstruct.js
@@ -1,17 +1,16 @@
 // Breakdown:
-// 1. split the given string and store it in a variable called strSplit
-// 2. Create an empty array called constructArr
-// 3. Create an empty array called deconstructArr
-// 4. Create an empty string and store it in a variable called constructStr
+// 1. Create an empty array called constructArr
+// 2. Create an empty array called deconstructArr
+// 3. Create an empty string and store it in a variable called constructStr
 // FOR CONSTRUCT
-// 5. use for loop to loop over the given string
+// 4. use for loop to loop over the given string
 // ===> 1. constructStr += str[i]
 // ===> 2. push constructStr into constructArr
 // FOR DECONSTRUCT
-// 6. use for loop to loop over the given string => Start the loop from 1 till the i is < str.length
+// 5. use for loop to loop over the given string => Start the loop from 1 till the i is < str.length
 // ===> 1. slice the given string from 0 till the index of the current index in negative and store it in a variable called deconstructStr
 // ===> 2. Push it into an array called deconstructArr
-// 7. Create a variable called constructAndDeconstructArr and join constructArr and deconstructArr
+// 6. Create a variable called constructAndDeconstructArr and join constructArr and deconstructArr
 
 function constructDeconstruct(str) {
   const constructArr = [];
