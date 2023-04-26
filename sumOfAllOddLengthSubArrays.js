3637f3fe4c5552c671ba5548ee21d674c9904021
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-04-26 12:17:51 -0400

    Day9:  Sum of All Odd Length Subarrays

--- sumOfAllOddLengthSubArrays.js
+++ sumOfAllOddLengthSubArrays.js
@@ -1,37 +1,14 @@
-// Breakdown:
-// 1. Create a variable called sumOfOddLengthSubArr and initialize it to 0
-// 2. Use reduce to perform the addition on the given array and store it in a variable called sumOfOddLengthSubArr
-// 3. if(arr's length > 2)
-// ===> 1. use for loop over the given array
-// ======> For each array's value, add it to the sumOfOddLengthSubArr
-// ===> 2. Use another for loop to loop over the given array.
-// ======> 1. Use slice method to get the sub array of length 3 and store it in a variable called subArr
-// ======> 2. check if subArr.length > 2
-// =========> 3. Use reduce to perform the addition of all the array elements and add it to the sumOfOddLengthSubArr
-// 4. else
-// ===> return sumOfOddLengthSubArr
-// 5. return sumOfOddLengthSubArr
-
-var sumOddLengthSubarrays = function (arr) {
-  let sumOfOddLengthSubArr = 0;
-  sumOfOddLengthSubArr = arr.reduce((acc, num) => acc + num, 0);
-  if (arr.length > 2) {
-    if (arr.length % 2 === 0) {
-      sumOfOddLengthSubArr = 0;
-    }
-    for (let num of arr) {
-      sumOfOddLengthSubArr += num;
-    }
-    if (arr.length > 3) {
-      for (let i = 0; i <= arr.length - 3; i++) {
-        sumOfOddLengthSubArr += arr
-          .slice(i, i + 3)
-          .reduce((acc, num) => acc + num, 0);
+function sumOddLengthSubarrays(arr) {
+  let sum = 0;
+  for (let i = 1; i <= arr.length; i += 2) {
+    for (let j = 0; j <= arr.length - i; j++) {
+      for (let k = j; k < j + i; k++) {
+        sum += arr[k];
       }
     }
   }
-  return sumOfOddLengthSubArr;
-};
+  return sum;
+}
 
 // SOME TEST CASES ARE FAILING => NEED TO RETHINK THE SOLUTION
 
