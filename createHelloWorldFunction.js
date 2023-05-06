ed96933fd751afaa6d9ce8c34664f504d7162ea2
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-05 23:20:19 -0400

    Day18: Create Hello World Function

---
+++ createHelloWorldFunction.js
@@ -0,0 +1,7 @@
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  };
+};
+
+// Problem link: https://leetcode.com/problems/create-hello-world-function/description/?utm_campaign=PostD1&utm_medium=Post&utm_source=Post&gio_link_id=QPDw0kJR
