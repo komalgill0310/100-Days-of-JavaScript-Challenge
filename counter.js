18bd7691d97cd16baefa97f2c994526438da6a6b
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-05 23:29:40 -0400

    Day18: Counter

---
+++ counter.js
@@ -0,0 +1,7 @@
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+//Problem link: https://leetcode.com/problems/counter/?utm_campaign=PostD2&utm_medium=Post&utm_source=Post&gio_link_id=xogkVqBo
