243275a120f60c5e63f6a7ba5c17032663b15c3d
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-20 00:03:38 -0400

    solve the reverse prefix of word problem

--- reversePrefixOfWord.js
+++ reversePrefixOfWord.js
@@ -1,1 +1,12 @@
+var reversePrefix = function (word, ch) {
+  const indexOfCh = word.indexOf(ch);
+  if (indexOfCh === -1) return word;
+  const reverseStr = word
+    .slice(0, indexOfCh + 1)
+    .split("")
+    .reverse()
+    .join("");
+  return reverseStr + word.slice(indexOfCh + 1);
+};
+
 // Problem link: https://leetcode.com/problems/reverse-prefix-of-word/
