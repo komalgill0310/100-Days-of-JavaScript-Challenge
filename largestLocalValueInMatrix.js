171c4fec25808bb25292db0d7b47d406332b709c
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-22 22:31:32 -0400

    Day35: Cells with Odd Values in a Matrix

---
+++ largestLocalValueInMatrix.js
@@ -0,0 +1,18 @@
+var oddCells = function (m, n, indices) {
+  const incrementMatrix = new Array(m).fill().map(() => new Array(n).fill(0));
+  for (const indice of indices) {
+    const [rowIndex, colIndex] = indice;
+    //UPDATE ROW OF INCREMENTMATRIX
+    const incrementRow = incrementMatrix[rowIndex];
+    for (let i = 0; i < incrementRow.length; i++) {
+      incrementRow[i]++;
+    }
+    //UPDATE COLUMN OF INCREMENTMATRIX
+    for (let j = 0; j < incrementMatrix.length; j++) {
+      incrementMatrix[j][colIndex]++;
+    }
+  }
+  return incrementMatrix.flat(1).filter((num) => num % 2 != 0).length;
+};
+
+// Problem link: https://leetcode.com/problems/largest-local-values-in-a-matrix/
