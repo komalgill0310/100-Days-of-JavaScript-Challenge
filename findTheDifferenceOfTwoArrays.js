094192633fb7fd1c4c9ad7e44b6af3dbd532ea11
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-27 19:51:19 -0400

    Day40: Find the Difference of Two Arrays(Solve yesterday's problem

--- findTheDifferenceOfTwoArrays.js
+++ findTheDifferenceOfTwoArrays.js
@@ -1,18 +1,18 @@
 // Breakdown:
 // 1. Create an empty array and store it in a variable called diffOfArr
-// 2. Use map for the nums1 to find out the elements which does not exist in the second array and store it in a variable called answer1
-// 3. Use map for the nums2 to find out the elements which does not exist in the first array and store it in a variabel called answer2
-// 4. diffOfArr(answer1.slice(0,2))
-// 5. diffOfArr(answer2.slice(0,2))
-// 6. return diffOfArr
+// 2. Use filter for the nums1 to find out the elements which does not exist in the second array and store it in a variable called answer1
+// 3. Use filter for the nums2 to find out the elements which does not exist in the first array and store it in a variabel called answer2
+// 4. Push answer1 and answer2 into diffOfArr
+// 5. return diffOfArr
 
 var findDifference = function (nums1, nums2) {
   const diffOfArr = [];
-  const answer1 = nums1.map((num) => nums2.findIndex(num) === -1 && num);
-  const answer2 = nums2.map((num) => !nums1.findIndex(num) === -1 && num);
-  console.log(answer1, answer2);
-  diffOfArr.push(answer1.slice(0, 2));
-  diffOfArr.push(answer2.slice(0, 2));
+  nums1 = [...new Set(nums1)];
+  nums2 = [...new Set(nums2)];
+  let answer1 = nums1.filter((num) => !nums2.includes(num));
+  let answer2 = nums2.filter((num) => !nums1.includes(num));
+
+  diffOfArr.push(answer1, answer2);
   return diffOfArr;
 };
 
