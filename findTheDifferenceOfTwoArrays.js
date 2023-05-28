e1ab71bccbc8bba8ce3146a7dd38834112b51768
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-27 20:01:19 -0400

    update the breakdown instructions

--- findTheDifferenceOfTwoArrays.js
+++ findTheDifferenceOfTwoArrays.js
@@ -1,19 +1,16 @@
 // Breakdown:
-// 1. Create an empty array and store it in a variable called diffOfArr
-// 2. Use filter for the nums1 to find out the elements which does not exist in the second array and store it in a variable called answer1
-// 3. Use filter for the nums2 to find out the elements which does not exist in the first array and store it in a variabel called answer2
-// 4. Push answer1 and answer2 into diffOfArr
-// 5. return diffOfArr
+// 1. Create an empty array and store it in a variable called answer
+// 2. Use filter for the nums1 to find out the elements which does not exist in the second array
+// 3. Use filter for the nums2 to find out the elements which does not exist in the first array
+// 4. return answer
 
 var findDifference = function (nums1, nums2) {
-  const diffOfArr = [];
+  const answer = Array(2);
   nums1 = [...new Set(nums1)];
   nums2 = [...new Set(nums2)];
-  let answer1 = nums1.filter((num) => !nums2.includes(num));
-  let answer2 = nums2.filter((num) => !nums1.includes(num));
-
-  diffOfArr.push(answer1, answer2);
-  return diffOfArr;
+  answer[0] = nums1.filter((num) => !nums2.includes(num));
+  answer[1] = nums2.filter((num) => !nums1.includes(num));
+  return answer;
 };
 
 // Problem link: https://leetcode.com/problems/find-the-difference-of-two-arrays/
