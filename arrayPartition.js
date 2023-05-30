8f97152384b8eb4bc36128262c5d957282404343
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-29 21:21:01 -0400

    updated variable names

--- arrayPartition.js
+++ arrayPartition.js
@@ -1,10 +1,10 @@
 var arrayPairSum = function (nums) {
-  const sortedNums = [...nums].sort((a, b) => a - b);
-  const pairedArr = [];
-  for (let i = 0; i < sortedNums.length; i += 2) {
-    pairedArr.push([sortedNums[i], sortedNums[i + 1]]);
+  const sortedArray = [...nums].sort((a, b) => a - b);
+  const pairs = [];
+  for (let i = 0; i < sortedArray.length; i += 2) {
+    pairs.push([sortedArray[i], sortedArray[i + 1]]);
   }
-  return pairedArr.reduce((total, subArr) => total + Math.min(...subArr), 0);
+  return pairs.reduce((total, pair) => total + Math.min(...pair), 0);
 };
 
 // Problem link: https://leetcode.com/problems/array-partition/
