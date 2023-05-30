6c29257154582adbfc7313b6297f4ad9cabd85c9
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-29 21:37:58 -0400

    Day42: Find Numbers with Even Number of Digits

--- countingWordsWithAGivenPrefix.js
+++ countingWordsWithAGivenPrefix.js
@@ -2,7 +2,8 @@ var prefixCount = function (words, pref) {
   const prefLength = pref.length;
   return words.reduce(
     (matchCount, word) =>
-      matchCount + (word.slice(0, prefLength) === pref ? 1 : 0),
+      matchCount +
+      (word.length >= prefLength && word.slice(0, prefLength) === pref ? 1 : 0),
     0
   );
 };
