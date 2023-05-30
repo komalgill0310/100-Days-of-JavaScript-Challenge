f523108c925e66f6fc4320afd22a95895c186407
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-29 21:33:47 -0400

    updated variable name

--- countingWordsWithAGivenPrefix.js
+++ countingWordsWithAGivenPrefix.js
@@ -17,8 +17,24 @@ var prefixCount = function (words, pref) {
 var prefixCount = function (words, pref) {
   const prefLength = pref.length;
   return words.reduce(
+<<<<<<< HEAD
+    (matchCount, word) =>
+      matchCount +
+      (word.length >= prefLength && word.slice(0, prefLength) === pref ? 1 : 0),
+=======
     (totalMatchedStr, word) =>
       totalMatchedStr + (word.slice(0, prefLength) === pref ? 1 : 0),
+>>>>>>> b4bdc15
+    0
+  );
+};
+
+// Problem link: https://leetcode.com/problems/counting-words-with-a-given-prefix/
+var prefixCount = function (words, pref) {
+  const prefLength = pref.length;
+  return words.reduce(
+    (matchCount, word) =>
+      matchCount + (word.slice(0, prefLength) === pref ? 1 : 0),
     0
   );
 };
