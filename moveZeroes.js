801b61fe11c695b386a2c27d48369e3404f08ea4
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-05-31 21:01:46 -0400

    Day78: Move Zeroes(solved it using two pointer approach)

--- moveZeroes.js
+++ moveZeroes.js
@@ -10,4 +10,23 @@ var moveZeroes = function (nums) {
   nums.push(...zeroesArr);
 };
 
+// BETTER APPROACH: Using two pointer
+
+var moveZeroes = function (nums) {
+  let nonZeroIndex = 0;
+
+  // MOVE ALL NON ZERO ELEMENTS AT THE BEGINNING OF AN ARRAY
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[nonZeroIndex] = nums[i];
+      nonZeroIndex++;
+    }
+  }
+
+  //  REPLACE REMAINING ELEMENTS WITH ZEROES
+  for (let i = nonZeroIndex; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+};
+
 // Problem link: https://leetcode.com/problems/move-zeroes/description/
