21f5e2b47cbf0502af7d5041d9c47bfd140e86be
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-06-06 21:58:25 -0400

    updated variable name

--- numberOfStudentsDoingHomeworkAtAGivenTime.js
+++ numberOfStudentsDoingHomeworkAtAGivenTime.js
@@ -1,9 +1,9 @@
 var busyStudent = function (startTime, endTime, queryTime) {
-  let numOfStudents = 0;
+  let studentCount = 0;
   for (let i = 0; i < startTime.length; i++) {
-    numOfStudents += queryTime >= startTime[i] && queryTime <= endTime[i] && 1;
+    studentCount += queryTime >= startTime[i] && queryTime <= endTime[i] && 1;
   }
-  return numOfStudents;
+  return studentCount;
 };
 
 // Problem link: https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
