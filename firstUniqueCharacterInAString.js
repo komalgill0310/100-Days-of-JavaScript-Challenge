6a64739f03019f537cff6fcd2f7d5598012e7dcc
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-06-18 22:57:23 -0400

    Day62: First Unique Character in a String

---
+++ firstUniqueCharacterInAString.js
@@ -0,0 +1,10 @@
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// Problem link: https://leetcode.com/problems/first-unique-character-in-a-string/description/
