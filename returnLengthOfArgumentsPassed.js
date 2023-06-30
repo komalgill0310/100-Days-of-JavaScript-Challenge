a9f0690da448d1b510df56c40f3a1fcca3618a08
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-06-29 23:21:31 -0400

    Day72: Return Length of Arguments Passed

---
+++ returnLengthOfArgumentsPassed.js
@@ -0,0 +1,5 @@
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+// Problem link: https://leetcode.com/problems/return-length-of-arguments-passed/description/?envType=study-plan-v2&envId=30-days-of-javascript
