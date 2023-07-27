c233afa7f72544b51422ccf961594373285c9d9f
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-27 10:33:56 -0400

    Day67: Relative Sort Array(yesterday's problem solved)

--- relativeSortArray.js
+++ relativeSortArray.js
@@ -1,17 +1,19 @@
 var relativeSortArray = function (arr1, arr2) {
+  let count = 0;
   for (let i = 0; i < arr2.length; i++) {
     const num2 = arr2[i];
     for (let j = 0; j < arr1.length; j++) {
       const num1 = arr1[j];
       if (num2 === num1) {
         arr1.splice(j, 1); //remove current element from the array
-        arr1.splice(i, 0, num2); //add current element at the index i
+        arr1.splice(count, 0, num2); //add current element at the index i
+        count++; //it will keep track of the duplicate elements which are equal to num2
       }
     }
   }
-  console.log(arr1);
+  const sortNonArr2Elements = arr1.splice(count).sort((a, b) => a - b);
+  arr1.push(...sortNonArr2Elements);
+  return arr1;
 };
-
-// yet to solve, bugs and this solution is failing
 
 // Problem link: https://leetcode.com/problems/relative-sort-array/
