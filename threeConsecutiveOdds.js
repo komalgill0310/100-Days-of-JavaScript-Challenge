e2a7250d8faaf35c601dd0e5a080cddfdffa79d0
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-08-02 12:02:27 -0400

    Day73: Three Consecutive Odds

---
+++ threeConsecutiveOdds.js
@@ -0,0 +1,10 @@
+var threeConsecutiveOdds = function (arr) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Problem link: https://leetcode.com/problems/three-consecutive-odds/description/
