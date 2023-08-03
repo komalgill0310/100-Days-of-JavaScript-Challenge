f0f3d1d27c6983e92fdc050eb22ba47542251a49
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-08-03 10:52:49 -0400

    Day74: Contains Duplicate

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,27 +1,13 @@
-// High Level Overview:
-// 1. use map to find the duplicate numbers, and return their indices
-// 2. Store the first and last index of duplicates
-// 3. Subtract the (last index - first index) <= k
-
-// Steps:
-// 1. Create an object called lastIndex
-// 2. Loop through an array using for loop
-// 3. store current num in an array at index i in a variable called num
-// 4. check if the num is a key in lastIndex object and if it is then check if i - lastIndex[num] <=k
-// ===> return true
-// 5. lastIndex[num] = i
-// 6. return false
-
-var containsNearbyDuplicate = function (nums, k) {
-  const lastIndex = {};
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (lastIndex.hasOwnProperty(num) && i - lastIndex[num] <= k) {
+var containsDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 1; i++) {
+    const prev = nums[i];
+    const next = nums[i + 1];
+    if (prev === next) {
       return true;
     }
-    lastIndex[num] = i;
   }
   return false;
 };
 
-// Problem link: https://leetcode.com/problems/contains-duplicate-ii/
+// Problem link: https://leetcode.com/problems/contains-duplicate/description/
