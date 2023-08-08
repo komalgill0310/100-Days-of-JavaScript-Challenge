3d6bbd0feaf8d3319d84bb29cedac8af3a693ef0
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-08-07 23:34:55 -0400

    Day76: Sort Even and Odd Indices Independently(solved the problem)

--- sortEvenAndOddIndicesIndependently.js
+++ sortEvenAndOddIndicesIndependently.js
@@ -1,14 +1,13 @@
 var sortEvenOdd = function (nums) {
-  nums.sort((a, b, i) => {
-    if (i % 2 === 0) {
-      return a[i] - b[i + 2];
-    } else {
-      return b[i] - a[i + 2];
-    }
-  });
-  console.log("nums: ", nums);
+  const evenArr = nums.filter((_, i) => i % 2 === 0).sort((a, b) => a - b);
+  const oddArr = nums.filter((_, i) => i % 2 != 0).sort((a, b) => b - a);
+  let evenIndex = 0,
+    oddIndex = 0;
+  nums.forEach(
+    (_, i) =>
+      (nums[i] = i % 2 === 0 ? evenArr[evenIndex++] : oddArr[oddIndex++])
+  );
+  return nums;
 };
-
-// YET TO SOLVE => WRONG THINKING
 
 // Problem link: https://leetcode.com/problems/sort-even-and-odd-indices-independently/
