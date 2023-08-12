3cf2bd709c06c5880741c9c266a9c4afe7b64966
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-08-12 19:56:35 -0400

    Day77: Two Sum

--- twoSum.js
+++ twoSum.js
@@ -1,3 +1,5 @@
+// OLD WAY
+
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
@@ -9,6 +11,20 @@ var twoSum = function (nums, target) {
     if (nums[i] === diff) {
       if (i != nums.lastIndexOf(diff)) {
         return [i, nums.lastIndexOf(diff)];
+      }
+    }
+  }
+};
+
+// NEW WAY
+
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    let curr = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      const next = nums[j];
+      if (curr + next === target) {
+        return [i, j];
       }
     }
   }
