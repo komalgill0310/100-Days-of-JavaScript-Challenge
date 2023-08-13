dabd38cc1e533b3a97d42676202e274d760152fa
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-08-12 20:57:38 -0400

    Day77: Remove Duplicates from Sorted Array(solve the problem with a different approach)

--- removeDuplicatesFromSortedArray.js
+++ removeDuplicatesFromSortedArray.js
@@ -1,3 +1,5 @@
+// OLD WAY
+
 var removeDuplicates = function (nums) {
   for (let i = 0; i < nums.length - 1; i++) {
     if (nums[i] === nums[i + 1]) {
@@ -5,6 +7,22 @@ var removeDuplicates = function (nums) {
       i--;
     }
   }
+};
+
+// A BETTER APPROACH
+
+var removeDuplicates = function (nums) {
+  let uniqueCount = 1;
+  j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    if (element !== nums[j]) {
+      j++;
+      nums[j] = nums[i];
+      uniqueCount++;
+    }
+  }
+  return uniqueCount;
 };
 
 // Problem link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
--- squaresOfASortedArray.js
+++ squaresOfASortedArray.js
@@ -2,4 +2,28 @@ var sortedSquares = function (nums) {
   return nums.map((num) => Math.pow(num, 2)).sort((a, b) => a - b);
 };
 
+// Better Approach: Two pointer approach
+
+var sortedSquares = function (nums) {
+  const numsLength = nums.length;
+  const sortedSquareArr = Array(numsLength);
+  let left = 0;
+  right = numsLength - 1;
+  index = numsLength - 1;
+  while (left <= right) {
+    const leftSquared = nums[left] * nums[left];
+    const rightSquared = nums[right] * nums[right];
+
+    if (leftSquared > rightSquared) {
+      sortedSquareArr[index] = leftSquared;
+      left++;
+    } else {
+      sortedSquareArr[index] = rightSquared;
+      right--;
+    }
+    index--;
+  }
+  return sortedSquareArr;
+};
+
 // Problem link: https://leetcode.com/problems/squares-of-a-sorted-array/
