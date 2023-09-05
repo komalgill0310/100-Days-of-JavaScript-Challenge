483899b5b67d9847f0578e9092a8bb4af27a6135
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-09-05 11:58:50 -0400

    Day91: Sort Colors(without in built method -> solution)

--- sortColors.js
+++ sortColors.js
@@ -3,3 +3,17 @@ var sortColors = function (nums) {
 };
 
 // Problem link: https://leetcode.com/problems/sort-colors/description/
+
+//  without in built method
+
+var sortColors = function (nums) {
+  const deepCopyOfNums = [...nums];
+  let j = 0;
+  for (let i = 0; i < deepCopyOfNums.length; i++) {
+    const min = Math.min(...deepCopyOfNums);
+    const minIndex = deepCopyOfNums.indexOf(min);
+    nums[j] = min;
+    deepCopyOfNums.splice(minIndex, 1);
+    i--, j++;
+  }
+};
