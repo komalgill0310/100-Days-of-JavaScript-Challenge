f63ec44aeac445f94479e64cf89569caff78bfd6
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-09-06 13:20:13 -0400

    Day92: Average Value of Even Numbers That Are Divisible by Three

---
+++ averageValueOfEvenNumbersThatAreDivisibleByThree.js
@@ -0,0 +1,10 @@
+var averageValue = function (nums) {
+  const divisibleBy3 = nums.filter((num, i) => num % 2 === 0 && num % 3 === 0);
+  const length = divisibleBy3.length;
+  const avg = length
+    ? divisibleBy3.reduce((total, curr) => total + curr, 0) / length
+    : length;
+  return Math.floor(avg);
+};
+
+// Problem link: https://leetcode.com/problems/average-value-of-even-numbers-that-are-divisible-by-three/description/
