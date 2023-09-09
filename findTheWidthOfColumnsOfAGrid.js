c94dd2c50744c394f09fc49f1004d57ccdac0d9e
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-09-09 13:34:32 -0400

    Day94: Find the width of Columns of a Grid(solved it)

--- findTheWidthOfColumnsOfAGrid.js
+++ findTheWidthOfColumnsOfAGrid.js
@@ -1,33 +1,18 @@
 // Incorrect solution: Still some cases are failing
 
 var findColumnWidth = function (grid) {
+  const rowLength = grid.length;
+  const colLength = grid[0].length;
   const width = [];
-  const m = grid.length;
-  for (let i = 0; i < m; i++) {
-    const col = [];
-    const n = grid[i].length;
-    for (let j = 0; j < n; j++) {
-      if (m === n) {
-        console.log(`m===n ${grid[j][i]}`);
-        const num = grid[j][i];
-        const numLength = String(num).split("").length;
-        col.push(numLength);
-      } else {
-        console.log(`m!=n ${grid[i][j]}`);
-        const num = grid[i][j];
-        const numLength = String(num).split("").length;
-        width.push(numLength);
-      }
+  for (let col = 0; col < colLength; col++) {
+    const eachColumn = [];
+    for (let row = 0; row < rowLength; row++) {
+      const numLength = String(grid[row][col]).split("").length;
+      eachColumn.push(numLength);
     }
-    if (m === n) {
-      width.push(Math.max(...col));
-    }
+    width.push(Math.max(...eachColumn));
   }
-  if (m != grid[0].length) {
-    return [Math.max(...width)];
-  } else {
-    return width;
-  }
+  return width;
 };
 
 // Problem link: https://leetcode.com/problems/find-the-width-of-columns-of-a-grid/
